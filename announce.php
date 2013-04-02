b0e0dbb335d7c8999d205f65a367a5f7198b7e2d
Alexey Shildyakov <ashl1future@gmail.com>
2013-04-03 02:16:59 +0400

    Fully rewrite announce script. Work with Redis NoSQL DB Doesn't implement statistic and grouping

--- announce.php
+++ announce.php
@@ -1,256 +1,201 @@
 <?php
 
+/*
+ * Supported BEPs (http://www.bittorrent.org/beps/bep_0000.html):
+ * 
+ *  BEP003: The BitTorrent Protocol Specification (http://www.bittorrent.org/beps/bep_0003.html)
+ *  BEP023: Tracker Returns Compact Peer Lists (http://www.bittorrent.org/beps/bep_0023.html)
+ */
+
+/*
+ *  !!!!!! REQUIREMENTS !!!!!!
+ *  - PHP > 5.3.0 (due to Predis)
+ *  - Redis >2.0.0 (due to SETEX command)
+ *  - Predis >0.6.0 (due to SETEX command)
+ */
+
+/*
+ *                  Redis Structure
+ * --------------------------------------------------------------------------------------------------
+ * |  peers:info_hash - Hash {peerId:ip:info_hash = 1}
+ * |  torrent:peerId:ip:info_hash - Expired string json([peer_id, ip, port])
+ * |  
+ */
+
+require 'Predis/Autoloader.php';
 include_once("BEncode.php");
 
-# Данные для доступа к БД MySQL
+// in seconds
+define('MIN_ANNOUNCE_INTERVAL_SEC', 60);
+define('RECOMMEND_ANNOUNCE_INTERVAL_SEC', 90);
+define('EXPIRE_TIME_SEC', 300);
 
-$DBHost='localhost';
-$DBUser='retracker_user';
-$DBPass='';
-$DBName='retracker';
+// Check input variables
+// TODO: log unknown field
 
-# -------------- Дальше ничего менять не надо! --------------------------------
-
-define('TIMESTART', utime());
-define('TIMENOW',   time());
-
-$announce_interval = 1800;
-$expire_factor     = 4;
-$peer_expire_time  = TIMENOW - floor($announce_interval * $expire_factor);
-
-mysql_connect("$DBHost","$DBUser","$DBPass") or msg_die("Could not connect: " . mysql_error());
-mysql_select_db("$DBName");
-
-mysql_query("DELETE FROM tracker WHERE update_time < $peer_expire_time")
- or msg_die("1. MySQL error: " . mysql_error());
-
-// Input var names
-// String
-//	'info_hash',
-//	'peer_id',
-
-$input_vars_str = array(
-	'event',
-	'descr',
-	'mt',
-	'pu',
-);
-// Numeric
-$input_vars_num = array(
-	'port',
-	'uploaded',
-	'downloaded',
-	'left',
-	'numwant',
-	'compact',
-	'l',
+$inputVarNames_EscapedStrings = array (
+    'info_hash',
+    'peer_id',
 );
 
-// Init received data
-$info_hash = urldecode($_GET['info_hash']);
-$peer_id = urldecode($_GET['peer_id']);
+$inputVarNames_Strings = array(
+        'event',
+);
+// ignore 'ip' field
+$inputVarNames_Numbers = array(
+        'compact',
+        'downloaded',
+        'left',
+        'port',
+        'uploaded',
+);
 
-// String
-foreach ($input_vars_str as $var_name)
-{
-	$$var_name = isset($_GET[$var_name]) ? (string) $_GET[$var_name] : null;
-}
-// Numeric
-foreach ($input_vars_num as $var_name)
-{
-	$$var_name = isset($_GET[$var_name]) ? (float) $_GET[$var_name] : null;
-}
+InitInputVariables();
+VerifyInputVariables();
 
-// Verify required request params (info_hash, peer_id, port, uploaded, downloaded, left)
-if (!isset($info_hash) || strlen($info_hash) != 20)
-{
-	// Похоже, к нам зашли через браузер.
-	// Вежливо отправим человека на инструкцию по псевдотрекеру.
-	echo "<meta http-equiv=refresh content=0;url=/pseudotracker/>";
-	die;
-//	msg_die("Invalid info_hash: '$info_hash' length ".strlen($info_hash));
-}
-if (!isset($peer_id) || strlen($peer_id) != 20)
-{
-	msg_die('Invalid peer_id');
-}
-if (!isset($port) || $port < 0 || $port > 0xFFFF)
-{
-	msg_die('Invalid port');
-}
-if (!isset($uploaded) || $uploaded < 0)
-{
-	msg_die('Invalid uploaded value');
-}
-if (!isset($downloaded) || $downloaded < 0)
-{
-	msg_die('Invalid downloaded value');
-}
-if (!isset($left) || $left < 0)
-{
-	msg_die('Invalid left value');
+try {
+    Predis\Autoloader::register();
+    $redis = new Predis\Client();
+} catch (Exception $e) {
+    SendError('1. Couldn\'t connect to DB');
 }
 
-// IP
-$ip = $_SERVER['REMOTE_ADDR'];
-
-
-// unknown code
-/*if (!$tr_cfg['ignore_reported_ip'] && isset($_GET['ip']) && $ip !== $_GET['ip'])
-{
-	if (!$tr_cfg['verify_reported_ip'])
-	{
-		$ip = $_GET['ip'];
-	}
-	else if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches))
-	{
-		foreach ($matches[0] as $x_ip)
-		{
-			if ($x_ip === $_GET['ip'])
-			{
-				if (!$tr_cfg['allow_internal_ip'] && preg_match("#^(10|172\.16|192\.168)\.#", $x_ip))
-				{
-					break;
-				}
-				$ip = $x_ip;
-				break;
-			}
-		}
-	}
-}*/
-
-// Check that IP format is valid
-if (!verify_ip($ip))
-{
-	msg_die("Invalid IP: $ip");
-}
-// Convert IP to HEX format
-$ip_sql = encode_ip($ip);
-
-// ----------------------------------------------------------------------------
-// Start announcer
-//
-
-$main_tracker = "$mt";
-
-// Escape strings
-$info_hash = mysql_real_escape_string($info_hash);
-$port = mysql_real_escape_string($port);
-$peer_id = mysql_real_escape_string($peer_id);
-$descr = mysql_real_escape_string($descr);
-$main_tracker = mysql_real_escape_string($main_tracker);
-$pu = mysql_real_escape_string($pu);
-$left = mysql_real_escape_string($left);
-$downloaded = mysql_real_escape_string($downloaded);
-$l = mysql_real_escape_string($l);
-
-// Stopped event
-if ($event === 'stopped')
-{
-	mysql_query("DELETE FROM tracker WHERE info_hash = '$info_hash' AND ip = '$ip_sql' AND port = $port")
-	 or msg_die("2. MySQL error: " . mysql_error());
-
-	die();
+// update info
+$peer = array(
+        $inputVars['peer_id'],
+        $userIp,
+        $inputVars['port'],
+    );
+try{
+    $key = implode(':', array($inputVars['peer_id'], $userIp, $inputVars['info_hash']));
+    $redis->setex('torrent:' . $key, EXPIRE_TIME_SEC, json_encode($peer));
+    $redis->hset('peers:' . $inputVars['info_hash'], $key, 1);
+}catch (Exception $e) {
+    error_log($e . "\n For ip: " . $userIp . ' and query: ' . $_SERVER['QUERY_STRING']);
+    SendError('2. DB error');
 }
 
+// TODO: Switch to SET insted of SETEX in newer Redis version
 
-mysql_query("REPLACE INTO tracker SET info_hash = '$info_hash', ip = '$ip_sql', port = $port, peer_id='$peer_id',
- update_time = ". time() .", descr = '$descr', tracker = '$main_tracker',
- ip_real = '$ip', publisherurl = '$pu', pleft = '$left', downloaded = '$downloaded', length = '$l'")
- or msg_die("3. MySQL error: " . mysql_error());
-
-// Select peers
-
-$result = mysql_query("SELECT peer_id, ip, port FROM tracker WHERE info_hash = '$info_hash'")
- or msg_die("4. MySQL error: " . mysql_error());
-
-$peers_list = array();
-
-while ($row = mysql_fetch_array($result, MYSQL_NUM))
-{
-	$peers_list[] = array(
-		'peer id' => $row[0],
-		'ip'      => decode_ip($row[1]),
-		'port'    => intval($row[2]),
-	);;
+try {
+    $peersKeys = $redis->hkeys('peers:' . $inputVars['info_hash']);
+    $peers = $redis->mget(array_map(function ($value) {return 'torrent:' . $value;}, $peersKeys));
+}catch (Exception $e) {
+    error_log($e . "\n For ip: " . $userIp . ' and query: ' . $_SERVER['QUERY_STRING']);
+    SendError('3. DB error');
 }
 
-/*if ($compact_mode)
-{
-	// CANNOT DO THIS
-	$peers = '';
-
-	foreach ($rowset as $peer)
-	{
-		$peers .= pack('Nn', ip2long(decode_ip($peer['ip'])), $peer['port']);
-	}
+$peersOutput = array();
+$keysToDeleteByTimeout = array();
+for ($i = 0; $i < count($peers); $i = $i+1) {
+    if (empty($peers[$i])) {
+        $keysToDeleteByTimeout[] = $peersKeys[$i];
+        continue;
+    }
+    $peer = json_decode($peers[$i]);
+    if ($peer[0] === $inputVars['peer_id'])
+        continue;
+    if ($inputVars['compact']) {
+        $peersOutput[] = StrIpToBinary($peer[1]) . pack('n', $peer[2]);
+    } else {
+        $peersOutput[] = array(
+                'peer id'       => $peer[0],
+                'ip'              => $peer[1],
+                'port'           => intval($peer[2]),
+        );
+    }
 }
-else
-{ */
-//}
-
+if ($inputVars['compact']) {
+    $peersOutput = implode($peersOutput);
+}
+if (count($keysToDeleteByTimeout)) {
+    $redis->hdel('peers:' . $inputVars['info_hash'], $keysToDeleteByTimeout);
+}
+    
 // Generate output
-
 $output = array(
-	'interval'     => 300, // tracker config: announce interval (sec?)
-	'min interval' => 60, // tracker config: min interval (sec?)
-	'peers'        => $peers_list,
+	'interval'     => RECOMMEND_ANNOUNCE_INTERVAL_SEC,
+	'min interval' => MIN_ANNOUNCE_INTERVAL_SEC, // ?
+	'peers'        => $peersOutput,
 );
 
 // Return data to client
 echo BEncode($output);
-
 exit;
 
 // ----------------------------------------------------------------------------
 // Functions
 //
-function utime ()
-{
-	return array_sum(explode(' ', microtime()));
+
+function BinIpToStr ($ip) {
+    return sprintf('%3u.%3u.%3u.%3u', $ip[0], $ip[1], $ip[2], $ip[3]);
 }
 
-function msg_die ($msg)
-{
-	$output = bencode(array(
-		'min interval'    => (int) 60,
-		'failure reason'  => (string) $msg,
-	));
-	
-	error_log($msg);
-
-	die($output);
+function InitInputVariables() {
+    global $inputVarNames_EscapedStrings,
+               $inputVarNames_Strings,
+               $inputVarNames_Numbers,
+               $inputVars;
+               
+    foreach ($inputVarNames_EscapedStrings as $var_name) {
+        $inputVars[$var_name] = isset($_GET[$var_name]) ?  urldecode($_GET[$var_name]) : null;
+    }
+    foreach ($inputVarNames_Strings as $var_name) {
+        $inputVars[$var_name] = isset($_GET[$var_name]) ? (string) $_GET[$var_name] : null;
+    }
+    foreach ($inputVarNames_Numbers as $var_name) {
+        $inputVars[$var_name] = isset($_GET[$var_name]) ? (float) $_GET[$var_name] : null;
+    }
+    
+    // FIXME: verify to not SQL injection for NoSQL
+    
+    global $userIp;
+    $userIp = $_SERVER['REMOTE_ADDR'];
+    
+    if (!isset($inputVars['compact'])) {
+        // in according to BEP023
+        $inputVars['compact'] = 1;
+    }        
 }
 
-function dummy_exit ($interval = 60)
-{
-	$output = bencode(array(
-		'interval'     => (int)    $interval,
-		'min interval' => (int)    $interval,
-		'peers'        => (string) DUMMY_PEER,
-	));
+function SendError ($msg) {
+        $output = bencode(array(
+                'min interval'    => (int) MIN_ANNOUNCE_INTERVAL_SEC,
+                'failure reason'  => (string) $msg,
+        ));
+        
+        error_log($msg);
 
-	die($output);
+        exit($output);
 }
 
-function encode_ip ($ip)
-{
-	$d = explode('.', $ip);
-
-	return sprintf('%02x%02x%02x%02x', $d[0], $d[1], $d[2], $d[3]);
+function StrIpToBinary ($ip) {
+        $d = explode('.', $ip);
+        return sprintf('%c%c%c%c', $d[0], $d[1], $d[2], $d[3]);
 }
 
-function decode_ip ($ip)
-{
-	return long2ip("0x{$ip}");
-}
-
-function verify_ip ($ip)
-{
-	return preg_match('#^(\d{1,3}\.){3}\d{1,3}$#', $ip);
-}
-
-function str_compact ($str)
-{
-	return preg_replace('#\s+#', ' ', trim($str));
+function VerifyInputVariables () {
+    global $inputVars;
+    if (!isset($inputVars['info_hash']) || strlen($inputVars['info_hash']) != 20) {
+            // Похоже, к нам зашли через браузер.
+            // Вежливо отправим человека на инструкцию по псевдотрекеру.
+            echo "<meta http-equiv=refresh content=0;url=/pseudotracker/>";
+            exit();
+    //      SendError("Invalid info_hash: '$info_hash' length ".strlen($info_hash));
+    }
+    if (!isset($inputVars['peer_id']) || strlen($inputVars['peer_id']) != 20) {
+            SendError('Invalid peer_id');
+    }
+    if (!isset($inputVars['port']) || $inputVars['port'] < 0 || $inputVars['port'] > 0xFFFF) {
+            SendError('Invalid port');
+    }
+    if (!isset($inputVars['uploaded']) || $inputVars['uploaded'] < 0) {
+            SendError('Invalid uploaded value');
+    }
+    if (!isset($inputVars['downloaded']) || $inputVars['downloaded'] < 0) {
+            SendError('Invalid downloaded value');
+    }
+    if (!isset($inputVars['left']) || $inputVars['left'] < 0) {
+            SendError('Invalid left value');
+    }
 }
